e97938409de4a1fe71d723dd0498185cd05ca765
Jeanne Lane <jeanne.m.lane8@gmail.com>
2025-08-03 22:57:56 -0400

    formatting

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,31 +1,33 @@
-import Head from "next/head"
-import NewMeetupForm from "../../components/meetups/NewMeetupForm"
-import { useRouter } from "next/router"
+import Head from "next/head";
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
 
 function NewMeetupPage() {
-    const router = useRouter()
-    async function addMeetupHandler(enteredMeetupData){
-        console.log(enteredMeetupData)
-        const response = await fetch("/api/new-meetup", {
-            method: "POST",
-            body: JSON.stringify(enteredMeetupData),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-        const data = await response.json()
-        console.log(data)
-        router.push("/")
-
-    }
-    return <>
-    
-    <Head>
+  const router = useRouter();
+  async function addMeetupHandler(enteredMeetupData) {
+    console.log(enteredMeetupData);
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    router.push("/");
+  }
+  return (
+    <>
+      <Head>
         <title>Add a New Meetup</title>
-        <meta name="description" content="Add your own meetups and create your own networking opportunities" />
-    </Head>
-    <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+        <meta
+          name="description"
+          content="Add your own meetups and create your own networking opportunities"
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </>
-
+  );
 }
-export default NewMeetupPage
+export default NewMeetupPage;
